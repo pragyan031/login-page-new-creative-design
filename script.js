ceb4f4765c2ed4f3bb65de917a917b27bef2ae0a
pragyan031 <124286405+pragyan031@users.noreply.github.com>
2024-01-24 05:39:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,12 +22,7 @@ const audio = new Audio(
 const music = new Audio(
   "https://dl.dropbox.com/scl/fi/is2za6upsjs3uk1ijodtv/Shubh-King-Shit-Official-Audio.mp3?rlkey=bf7oytc6na5kgorn1znuzc41p&dl=0"
 );
-let acc = confirm("Already have account?");
-if(acc == true){
-setTimeout(() => {
   music.play();
-}, 5000);
- } 
 // setInterval();
 function validateForm() {
   // check if the user name is empty or not
